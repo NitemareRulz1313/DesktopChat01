eded7abc5b444a902a64df0966266d9783f7fd83
NitemareRulz1313 <NitemareRulz1313@gmail.com>
2020-11-30 23:42:02 +0000

    -Added the Mainwindow. -Styled the MainWindow. -Added the commands for Minimize,Maximize and Close.

---
+++ DesktopChat01/DesktopChat01/DataModels/ApplicationPage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopChat01.DataModels
+{
+   public enum ApplicationPage
+    {
+        Login
+      
+    }
+}
